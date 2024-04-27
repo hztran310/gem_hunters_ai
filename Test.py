e956614be14c0d3a77bfadaddd09b30a1e5302ea
ntkhoang <142283277+ntkhoang@users.noreply.github.com>
2024-04-27 18:30:33 +0700

    Refactor generate_cnf function to improve readability and remove unnecessary code

--- Test.py
+++ Test.py
@@ -11,21 +11,19 @@ def generate_cnf(grid):
   cnf = []
 
   for i in range(height):
-      for j in range(width):
-          if isinstance(grid[i][j], int):
-              # Get the neighbors of the cell
-              neighbors = [(x, y) for x in range(i-1, i+2) for y in range(j-1, j+2) 
-                            if 0 <= x < height and 0 <= y < width and (x, y) != (i, j) and grid[x][y] is None]
-              
-              # Generate clauses for exactly the number of traps
-              for subset in combinations(neighbors, grid[i][j]):
-                  clause = [neighbor[0]*width + neighbor[1] + 1 for neighbor in subset]
-                  cnf.append(clause)
-              
-              # Generate clauses for more than the number of traps
-              for subset in combinations(neighbors, grid[i][j] + 1):
-                  clause = [-1*(neighbor[0]*width + neighbor[1] + 1) for neighbor in subset]
-                  cnf.append(clause)
+    for j in range(width):
+      if isinstance(grid[i][j], int):
+        # Get the neighbors of the cell
+        neighbors = [(x, y) for x in range(i-1, i+2) for y in range(j-1, j+2) 
+             if 0 <= x < height and 0 <= y < width and (x, y) != (i, j) and grid[x][y] is None]
+        
+        # Generate clauses for exactly the number of traps
+        for subset in combinations(neighbors, grid[i][j]):
+          clause = [(neighbor[0]*width + neighbor[1] + 1) for neighbor in subset]
+          cnf.append(clause)
+
+  # Remove duplicate clauses
+  cnf = [list(x) for x in set(tuple(x) for x in cnf)]
 
   return cnf
 
@@ -38,14 +36,24 @@ def solve(cnf, grid):
         solver.add_clause(clause)
 
     if solver.solve():
-        model = solver.get_model()
-        solution = [['T' if i*width + j + 1 in model else 'G' for j in range(width)] for i in range(height)]
-        print("Input:")
-        print_grid(grid)
-        print("Output:")
-        print_grid(solution)
+      model = solver.get_model()
+      for i in range(height):
+        for j in range(width):
+          if grid[i][j] is None:
+            check = False
+            for k in range(1, height*width+1):
+              if k in model and k == i*width + j + 1:
+                  grid[i][j] = 'G'
+                  check = True
+                  break
+                
+            if not check:
+              grid[i][j] = 'T'
+                        
+      print("Output:")
+      print_grid(grid)
     else:
-        print("No solution found")
+      print("No solution found")
 
     return None
 
